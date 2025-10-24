223a4cb658fc17e5a589fc9c2b8c140703d61478
Kamal088 <kamalprajapati4402@gmail.com>
2025-10-24 16:28:52 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
- 1    print("Cybersecurity Portfolio - K
-2    print("BCA with Cybersecurity")
-3    print("Building Security Tools")
-4
-5    # Simple security demo
-6    password = input("Enter password to c
-7    if len(password) >= 8:
-8    print("Strong: 8+ characters")
-9    else:
-10    print("Weak: Use 8+ characters"
+print("Cybersecurity Portfolio - Kamal088")
+print("BCA with Cybersecurity")
+print("Building Security Tools")
+
+# Simple security demo
+password = input("Enter password to check length: ")
+if len(password) >= 8:
+    print("Strong: 8+ characters")
+else:
+    print("Weak: Use 8+ characters")
